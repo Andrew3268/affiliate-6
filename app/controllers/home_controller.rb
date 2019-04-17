2260085a18b15d98814d2c38c76401b82749d280
Your <scarlet3628@likelion.org>
2019-04-18 01:02:05 +0900

    font

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,12 +1,12 @@
 class HomeController < ApplicationController
-  def index
-    @gadgets = Gadget.all.order("created_at DESC").page(params[:page]).per_page(21)
-  end
+  # def index
+  #   @gadgets = Gadget.all.order("created_at DESC").page(params[:page]).per_page(21)
+  # end
 
-  def index_01
-    @gadgets = Gadget.all.order("created_at DESC").page(params[:page]).per_page(21)
-    @recommends = Recommend.all.order("created_at DESC").limit(5)
-  end
+  # def index_01
+  #   @gadgets = Gadget.all.order("created_at DESC").page(params[:page]).per_page(21)
+  #   @recommends = Recommend.all.order("created_at DESC").limit(5)
+  # end
 
   def index_02
     @gadgets = Gadget.all.order("created_at DESC").page(params[:page]).per_page(50)
