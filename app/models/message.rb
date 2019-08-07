5a90ae4aace7e93c206427989c7b26edac158fd5
Your <scarlet3628@likelion.org>
2019-08-08 02:28:22 +0900

    codes

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,7 @@
 class Message < ActiveRecord::Base
   belongs_to :user
+  validates :mmail, :presence => true
+  # validates :m_spare_02, :presence => true
+  validates_format_of :mmail,:with => /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
+  
 end
