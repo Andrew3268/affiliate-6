554ecb311c89236794efb64b7f204c07ee7b2056
Your <scarlet3628@likelion.org>
2019-10-18 21:55:54 +0800

    hit counter for hotdeals

--- app/models/hotdeal.rb
+++ app/models/hotdeal.rb
@@ -4,15 +4,15 @@ class Hotdeal < ActiveRecord::Base
   has_many :impressions, :as=>:impressionable
   is_impressionable :counter_cache => true, :unique => true
 
-  def impression_count
-    impressions.size
-  end
+  # def impression_count
+  #   impressions.size
+  # end
 
-  def unique_impression_count
-    # impressions.group(:ip_address).size gives => {'127.0.0.1'=>9, '0.0.0.0'=>1}
-    # so getting keys from the hash and calculating the number of keys
-    impressions.group(:ip_address).size.keys.length #TESTED
-  end
+  # def unique_impression_count
+  #   impressions.group(:ip_address).size gives => {'127.0.0.1'=>9, '0.0.0.0'=>1}
+  #   # so getting keys from the hash and calculating the number of keys
+  #   # impressions.group(:ip_address).size.keys.length #TESTED
+  # end
 
   extend FriendlyId
   friendly_id :h_title, use: :slugged
