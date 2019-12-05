53f4ef6436c9fd4320ab9058ec0f11a9396e4766
Your <scarlet3628@likelion.org>
2019-12-05 23:54:34 +0900

    deals

--- app/controllers/deals_controller.rb
+++ app/controllers/deals_controller.rb
@@ -134,7 +134,7 @@ class DealsController < ApplicationController
 
   def all_deals
     @promocodes = Promocode.all.order("created_at DESC").limit(20)
-    @hotdeals = Hotdeal.all.order("created_at DESC").limit(30)
+    @hotdeals = Hotdeal.all.order("created_at DESC").limit(20)
     @blog_deals = Hotdeal.all.order("created_at DESC").limit(30)
   end
 
