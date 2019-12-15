19de1d6a3a35f9dae67ece6c8d60e9cd5b1addba
Your <scarlet3628@likelion.org>
2019-12-16 01:50:26 +0900

    deals

--- app/assets/javascripts/common.js
+++ app/assets/javascripts/common.js
@@ -205,5 +205,56 @@ $('document').ready(function () {
 });
 
 
-  
+// Start Amazon Slider //
+"use strict";
+  productScroll();
+  function productScroll() {
+    let slider = document.getElementById("slider");
+    let next = document.getElementsByClassName("pro-next");
+    let prev = document.getElementsByClassName("pro-prev");
+    let slide = document.getElementById("slide");
+    let item = document.getElementById("slide");
+
+    for (let i = 0; i < next.length; i++) {
+      let position = 0; //slider postion
+
+      prev[i].addEventListener("click", function() {
+        if (position > 0) {
+          position -= 1;
+          translateX(position); //translate items
+        }
+      });
+
+      next[i].addEventListener("click", function() {
+        if (position >= 0 && position < hiddenItems()) {
+          position += 1;
+          translateX(position); //translate items
+        }
+      });
+    }
+
+    function hiddenItems() {
+      let items = getCount(item, false);
+      let visibleItems = slider.offsetWidth / 210;
+      return items - Math.ceil(visibleItems);
+    }
+  }
+
+  function translateX(position) {
+    slide.style.left = position * -210 + "px";
+  }
+
+  function getCount(parent, getChildrensChildren) {
+    let relevantChildren = 0;
+    let children = parent.childNodes.length;
+    for (let i = 0; i < children; i++) {
+      if (parent.childNodes[i].nodeType != 3) {
+        if (getChildrensChildren)
+          relevantChildren += getCount(parent.childNodes[i], true);
+        relevantChildren++;
+      }
+    }
+    return relevantChildren;
+  }
+// End //  
 
