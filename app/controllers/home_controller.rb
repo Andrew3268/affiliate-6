4862da5400e46f1290233ed88e0e2e42e483ef53
Your <scarlet3628@likelion.org>
2019-12-17 06:20:04 +0900

    before adding sitemap generatior gem

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -16,7 +16,7 @@ class HomeController < ApplicationController
       @gadgets = @gadgets.search_by(@search_term)
     end
 
-   set_meta_tags  title: 'The Best Deals, Coupons, Promo Codes, Discounts',
+   set_meta_tags  title: 'Awesome Gadgets, The Best Deals, Coupons, Promo Codes, Discounts',
                   site: 'Oh,igottabuythis',
                   revierse: true,
                   description: 'It is discount platform but also trending item platform | Oh,igottabuythis is here to discover awesome gadgets. Find thousands of Amazon best deals, coupon codes, promotions you love.',
@@ -24,12 +24,12 @@ class HomeController < ApplicationController
                   twitter: {
                     card: "summary",
                     site: "@OhIgottabuythis",
-                    title: 'The Best Deals, Coupons, Promo Codes, Discounts',
+                    title: 'Awesome Gadgets, The Best Deals, Coupons, Promo Codes, Discounts',
                     description: 'It is discount platform but also trending item platform | Oh,igottabuythis is here to discover awesome gadgets. Find thousands of Amazon best deals, coupon codes, promotions you love.',
                     image: 'https://i.pinimg.com/564x/ff/c1/e5/ffc1e5c2a49a3ed756d47b0532916ca8.jpg',
                   },
                   og: {
-                    title: 'The Best Deals, Coupons, Promo Codes, Discounts',
+                    title: 'Awesome Gadgets, The Best Deals, Coupons, Promo Codes, Discounts',
                     description: 'It is discount platform but also trending item platform | Oh,igottabuythis is here to discover awesome gadgets. Find thousands of Amazon best deals, coupon codes, promotions you love.',
                     type: 'website',
                     url: 'http://www.ohigottabuythis.net',
