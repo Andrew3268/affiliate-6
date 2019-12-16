78651fda920f6ca478aff39cb69e5bc76be33d59
Your <scarlet3628@likelion.org>
2019-12-17 06:56:18 +0900

    sitemap2

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,5 +1,5 @@
 # Set the host name for URL creation
-SitemapGenerator::Sitemap.default_host = "http://www.example.com"
+SitemapGenerator::Sitemap.default_host = "http://www.ohigottabuythis.net"
 
 SitemapGenerator::Sitemap.create do
   # Put links creation logic here.
@@ -24,13 +24,15 @@ SitemapGenerator::Sitemap.create do
   #   Article.find_each do |article|
   #     add article_path(article), :lastmod => article.updated_at
   #   end
-  # add hotdeals_path, :priority => 0.5, :changefreq => 'daily'
-  # Hotdeal.find_each do |hotdeal|
-  #   add hotdeal_path(hotdeal), :lastmod => hotdeal.updated_at, :priority => 0.5
-  # end
 
-  add promocodes_path, :priority => 0.5, :changefreq => 'daily'
-  Promocode.find_each do |promocode| 
-    add promocode_path(promocode), :lastmod => promocode.updated_at, :priority => 0.5
+
+  add hotdeals_path, :priority => 0.5, :changefreq => 'daily'
+  Hotdeal.find_each do |hotdeal|
+    add hotdeal_path(hotdeal), :lastmod => hotdeal.updated_at
   end
+
+  # add promocodes_path, :priority => 0.5, :changefreq => 'daily'
+  # Promocode.find_each do |promocode| 
+  #   add promocode_path(promocode), :lastmod => promocode.updated_at, :priority => 0.5
+  # end
 end
