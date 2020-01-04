3911dced0bcf08aea4892845bb34b9da9ae9e4f7
Your <scarlet3628@likelion.org>
2020-01-04 21:26:36 +0900

    deals

--- app/assets/javascripts/common.js
+++ app/assets/javascripts/common.js
@@ -153,58 +153,6 @@ updateNav();
   //End
 
 
-//Start Google Translate WORK IN PROGRESS BELOW        
-$('document').ready(function () {
-  // RESTYLE THE DROPDOWN MENU
-  $('#google_translate_element').on("click", function () {
-    // Change font family and color
-    $("iframe").contents().find(".goog-te-menu2-item div, .goog-te-menu2-item:link div, .goog-te-menu2-item:visited div, .goog-te-menu2-item:active div, .goog-te-menu2 *")
-        .css({
-            'color': '#544F4B',
-            'font-family': 'Roboto',
-            'width':'70%'
-        });
-    // Change menu's padding
-    $("iframe").contents().find('.goog-te-menu2-item-selected').css ('display', 'none');
-  
-    // Change menu's padding
-    $("iframe").contents().find('.goog-te-menu2').css ('padding', '0px');
-  
-    // Change the padding of the languages
-    $("iframe").contents().find('.goog-te-menu2-item div').css('padding', '20px');
-  
-    // Change the width of the languages
-    $("iframe").contents().find('.goog-te-menu2-item').css('width', '70%');
-    $("iframe").contents().find('td').css('width', '70%');
-  
-    // Change hover effects
-    $("iframe").contents().find(".goog-te-menu2-item div").hover(function () {
-        $(this).css('background-color', '#4385F5').find('span.text').css('color', 'white');
-    }, function () {
-        $(this).css('background-color', 'white').find('span.text').css('color', '#544F4B');
-    });
-
-    // Change Google's default blue border
-    $("iframe").contents().find('.goog-te-menu2').css('border', 'none');
-
-    // Change the iframe's box shadow
-    $(".goog-te-menu-frame").css('box-shadow', '0 16px 24px 2px rgba(0, 0, 0, 0.14), 0 6px 30px 5px rgba(0, 0, 0, 0.12), 0 8px 10px -5px rgba(0, 0, 0, 0.3)');
-    
-    // Change the iframe's size and position?
-    $(".goog-te-menu-frame").css({
-        'height': '100%',
-        'width': '70%',
-        'top': '0px'
-    });
-    // Change iframes's size
-    $("iframe").contents().find('.goog-te-menu2').css({
-        'height': '100%',
-        'width': '70%'
-    });
-  });
-});
-
-
 // Start Amazon Slider //
 "use strict";
   productScroll();
@@ -256,5 +204,39 @@ $('document').ready(function () {
     }
     return relevantChildren;
   }
-// End //  
+// End // 
+
+
+
+  $(function() {
+  var Accordion = function(el, multiple) {
+    this.el = el || {};
+    // more then one submenu open?
+    this.multiple = multiple || false;
+    
+    var dropdownlink = this.el.find('.dropdownlink');
+    dropdownlink.on('click',
+                    { el: this.el, multiple: this.multiple },
+                    this.dropdown);
+  };
+  
+  Accordion.prototype.dropdown = function(e) {
+    var $el = e.data.el,
+        $this = $(this),
+        //this is the ul.submenuItems
+        $next = $this.next();
+    
+    $next.slideToggle();
+    $this.parent().toggleClass('open');
+    
+    if(!e.data.multiple) {
+      //show only one menu at the same time
+      $el.find('.submenuItems').not($next).slideUp().parent().removeClass('open');
+    }
+  }
+  
+  var accordion = new Accordion($('.accordion-menu'), false);
+})
+
+
 
